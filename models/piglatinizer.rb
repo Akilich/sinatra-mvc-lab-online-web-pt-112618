f44aea15092f28067e6ae4b5113750214e5dce54
Akili Humphrey <akilihumphrey@gmail.com>
2019-03-16 07:15:13 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,6 +6,15 @@ class PigLatinizer
     #user_phrase.split.map do |word|
     #  piglatinize_each_word
    # end.join(' ')
-   
+   vowels = %w{a e i o u}
+    word.each_char do |chr|
+    index = word.index(chr)
+    if index != 0 && vowels.include?(chr.downcase)
+      consonants = word.slice!(0..index-1)
+      return word + consonants + "ay"
+    elsif index == 0 && vowels.include?(chr.downcase)
+      return word + "ay"
+    end
   end
+end
   
