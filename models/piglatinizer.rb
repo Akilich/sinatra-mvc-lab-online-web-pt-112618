8cf735a94eb2fd32c80d66d54f5b7a383032309e
Akili Humphrey <akilihumphrey@gmail.com>
2019-03-16 07:10:54 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,23 @@
+class PigLatinizer
+  
+  def piglatinize(user_phrase)
+    user_phrase.split(" ").collect { |word| piglatinize(word) }.join(" ")
+  end
+   # user_phrase.split.map do |word|
+   #   piglatinize_each_word
+   # end.join(' ')
+  #end
+  
+  def piglatinize_each_word
+    vowels = %w{a e i o u}
+    word.each_char do |chr|
+    index = word.index(chr)
+    if index != 0 && vowels.include?(chr.downcase)
+      consonants = word.slice!(0..index-1)
+      return word + consonants + "ay"
+    elsif index == 0 && vowels.include?(chr.downcase)
+      return word + "ay"
+    end
+  end
+end
+end
