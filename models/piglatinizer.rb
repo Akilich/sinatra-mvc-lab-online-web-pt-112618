414479bad3bf6120657c7c0ffe8a38628050486a
Akili Humphrey <akilihumphrey@gmail.com>
2019-03-16 07:21:25 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,7 @@
 class PigLatinizer
   def piglatinize(user_phrase)
     user_input = user_phrase.split(" ")
-        split_string= user_string.collect {|word| piglatinize_word(word)}
+        split_phrase= user_string.collect {|word| piglatinize_word(word)}
 
         split_string.join(" ")
 end
